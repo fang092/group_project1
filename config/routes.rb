f106f03fe653934e3b9e3efc17586ad34f0eaf17
u1 <u1@192.168.0.2>
2014-11-16 12:35:34 -0500

    did some routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/spaghettiStack'
-  get 'static_pages/binaryTree'
-  get 'static_pages/graph'
+  root 'static_pages#home' as: :home
+  get 'static_pages/spaghettiStack' as: :spaghettiStack
+  get 'static_pages/binaryTree' as: :binaryTree
+  get 'static_pages/graph' as: :graph
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
